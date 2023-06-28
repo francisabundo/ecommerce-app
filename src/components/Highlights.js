b6d2e3b450d230840777a50e36decbc01a26081b
Benjikoy <francs.abundo@gmail.com>
2023-06-28 16:01:43 +0800

    migrated

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -4,8 +4,9 @@ import data from '../Data.js'
 export default function Highlights(){
     return (
         
-        <div className='mt-3'>
-            
+          //not used
+        <div>
+          
 
         </div>
 
